e32d87dc938192ba0d4155fd9c8de56121ded7a7
Kehinde Ogundele <Kehindegundel@gmail.com>
2023-09-14 09:12:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const fourthBox = document.querySelectorAll(".box-4");
 const display = document.querySelector(".display");
-const val = document.querySelector('input').value;
-
 
 fourthBox.forEach((box) => {
     box.addEventListener("click", (e) =>{

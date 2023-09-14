8992281ed5d63ced3d92c24b693c69b6e870ec11
doziechuks <doziechuks1010@gmail.com>
2023-09-14 10:03:44 +0100

    updated the main html and js files to add calculation functionality

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fourthBox = document.querySelectorAll(".box-4");
 const display = document.querySelector(".display");
+const equalBtn = document.querySelector("#equal");
+// console.log({ equalBtn });
 
 fourthBox.forEach((box) => {
-    box.addEventListener("click", (e) =>{
-        display.value += box.value;
-    })
-})
+  box.addEventListener("click", () => {
+    display.value += box.value;
+  });
+});
+
+equalBtn.addEventListener("click", () => {
+  display.value = eval(display.value);
+});

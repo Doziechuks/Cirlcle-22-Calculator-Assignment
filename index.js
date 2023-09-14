67240f8915eb058e744f8712336ea50bf9cb73a9
petroritse1 <petroritse@gmail.com>
2023-09-14 01:01:21 +0100

    made the app responsive with css

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello");
+
+
+// 
+const output = document.querySelector(".eval");
+const display = document.querySelector(".display");
+const box = document.querySelector(".box")
+const inpu = document.querySelector(".button")
+
+ 
+let calc = ""
+console.log(display.value)
+box.addEventListener("click",(e)=>{
+
+    calc += e.target.value
+    display.value = calc;
+   
+
+    
+})
+
+
+

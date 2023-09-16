fad0f96923aca50ac27be1121c5031ab186ef95c
Lucas Wilson <lucaslulyeho@gmail.com>
2023-09-16 10:56:20 +0300

    Adding functionality for box-3

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 const fourthBox = document.querySelectorAll(".box-4");
+const thirdBox = document.querySelectorAll(".box-3");
 const display = document.querySelector(".display");
 const equalBtn = document.querySelector("#equal");
 // console.log({ equalBtn });
 
+for(let box of thirdBox){
+  box.addEventListener("click", () => {
+    display.value += box.value;
+  });
+}
 fourthBox.forEach((box) => {
   box.addEventListener("click", () => {
     display.value += box.value;

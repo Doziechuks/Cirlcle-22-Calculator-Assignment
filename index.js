427a8e81ae981ab15b1f266022da7a2ee6536564
Adetutu Oluwasanmi <oluwasanmiadetutu@gmail.com>
2023-09-16 09:57:51 +0100

    added functionality to the 0, 00, and % buttons

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 const fourthBox = document.querySelectorAll(".box-4");
+const zeroBtn = document.querySelectorAll(".box-0");
 const display = document.querySelector(".display");
 const equalBtn = document.querySelector("#equal");
+const percentageBtn = document.querySelector("#percentage");
 // console.log({ equalBtn });
 
 fourthBox.forEach((box) => {
@@ -10,6 +12,20 @@ fourthBox.forEach((box) => {
   });
 });
 
+zeroBtn.forEach((box) => {
+  box.addEventListener("click", () => {
+    display.value += box.value;
+  });
+});
+
 equalBtn.addEventListener("click", () => {
   display.value = eval(display.value);
 });
+
+percentageBtn.addEventListener("click", () => {
+  const currentValue = parseFloat(display.value);
+  const percentage = currentValue / 100;
+  const result = percentage * 10; 
+  display.value = result;
+});
+
